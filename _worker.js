677d329a17fb97ea796aef09c61b6a00a24f1c4a
chodotdesign <159584698+chodotdesign@users.noreply.github.com>
2024-09-10 20:52:07 +0700

    PROXYIP America/Chicago

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'b3e83031-d633-46ea-b75c-a61649165ada';
 
-const พร็อกซีไอพีs = ['18.141.204.88'];
+const พร็อกซีไอพีs = ['45.60.186.91'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
